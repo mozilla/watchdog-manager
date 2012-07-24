b7c04584122d94c2733a4fd8e086b075d32b609e
Nathan Malkin <nmalkin@mozilla.com>
2012-07-24 15:43:31 -0700

    Add visual indicator of password strength
    Number of stars corresponds to password strength score.
    
    Score is computed using zxcvbn library.

--- data/js/manager.js
+++ data/js/manager.js
@@ -1,12 +1,33 @@
 var count = 0;
 
+/** Given a password, returns a visual representation of its strength */
+function strengthHTML(password) {
+    var STAR_FILLED = '&#9733;',
+        STAR_EMPTY  = '&#9734;';
+
+    var score = zxcvbn(password).score;
+
+    var html = '';
+    var i;
+
+    for(i = 0; i < score; i++) {
+        html += STAR_FILLED;
+    }
+
+    for(i = score; i < 4; i++) {
+        html += STAR_EMPTY;
+    }
+
+    return html;
+}
+
 function credentialHTML(credential) {
     return '<tr><td>' + (++count) + '</td><td>' +
         credential.site + '</td><td>' +
         credential.username + '</td><td>' +
         '<img src="' + getDataURLForHash(SHA1(credential.password),70,25) + '"/>' + '</td><td>' +
         credential.age + '</td><td>' +
-        credential.strength + '</td><td>' +
+        strengthHTML(credential.password) + '</td><td>' +
         '<input type="button" value="Change"></td>' +
         '</tr>';
 }
