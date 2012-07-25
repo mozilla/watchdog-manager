bb1c01145602f03f11f3e82c8c84754df77db032
Paul Sawaya <me@paulsawaya.com>
2012-07-24 17:37:49 -0700

    add data table

--- data/js/manager.js
+++ data/js/manager.js
@@ -1,15 +1,32 @@
 var count = 0;
 
+$(document).ready(function(){
+	getCredentials();
+});
+
 function credentialHTML(credential) {
     return '<tr><td>' + (++count) + '</td><td>' +
         credential.site + '</td><td>' +
         credential.username + '</td><td>' +
-        '<img src="' + getDataURLForHash(SHA1(credential.password),70,25) + '"/>' + '</td><td>' +
+        SHA1(credential.password) + '</td><td>' +
         credential.age + '</td><td>' +
         credential.strength + '</td><td>' +
         '<input type="button" value="Change"></td>' +
         '</tr>';
 }
-self.port.on('credential', function(data) {
-    document.getElementById('password-content').innerHTML += credentialHTML(data);
-});
+
+function addCredentials(credentials) {
+	credentials.forEach(function(credential) {
+	    $('#password-table tbody').append($(credentialHTML(credential)));
+	});
+	$('#password-table').dataTable({
+		'aoColumnDefs': [
+			{
+				'fnRender': function(obj, val) {
+					return '<img src="' + getDataURLForHash(val,70,25) + '"/>';
+				},
+				'aTargets': [3]
+			}
+		]
+	});
+}
