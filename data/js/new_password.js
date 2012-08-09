82c89b777320886da278abed7560c77dd09f0cb7
Nathan Malkin <nmalkin@mozilla.com>
2012-08-09 13:01:58 -0700

    Display dialog for creating new password
    when password change is triggered via alert

---
+++ data/js/new_password.js
@@ -0,0 +1,40 @@
+var passwordSection = $('#set-password');
+var passwordField = $('#password');
+
+/** Generate new password */
+$('#generate').click(function() {
+    var newPassword = (new Password()).toString();
+
+    passwordField.prop('disabled', 'disabled');
+    passwordField.val(newPassword);
+
+    passwordSection.show();
+});
+
+/** Enable manual entry of passwords */
+$('#manual').click(function() {
+    passwordField.prop('disabled', '');
+    passwordField.val('');
+
+    passwordSection.show();
+});
+
+/** Toggle visibility of password in field */
+var hidden = true;
+var unhideButton = $('#unhide');
+unhideButton.click(function() {
+    if(hidden) {
+        passwordField.prop('type', 'text');
+        unhideButton.val('Hide the password');
+    } else {
+        passwordField.prop('type', 'password');
+        unhideButton.val('Show it to me in plain text');
+    }
+
+    hidden = ! hidden;
+});
+
+/** Submit password */
+$('input[type=submit]').click(function() {
+   self.port.emit('usePassword', passwordField.val()); 
+});
---
+++ data/js/password.js
@@ -0,0 +1,30 @@
+var PASSWORD_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
+var PASSWORD_LENGTH = 16;
+
+function Password() {
+    /** Returns random integer on [0, upperBound) */
+    function randInt(upperBound) {
+        return Math.floor(Math.random() * upperBound);
+    }
+
+    /** Returns a random value from the array */
+    function selectRandom(array) {
+        return array[randInt(array.length)];
+    }
+
+    /** Returns a random password of given length using characters in CHARS */
+    function generateRandomPassword(length, chars) {
+        var password = '';
+        for(var i = 0; i < length; i++) {
+            password += selectRandom(chars);
+        }
+
+        return password;
+    }
+
+    this.password = generateRandomPassword(PASSWORD_LENGTH, PASSWORD_CHARS);
+}
+
+Password.prototype.toString = function() {
+    return this.password;
+}
